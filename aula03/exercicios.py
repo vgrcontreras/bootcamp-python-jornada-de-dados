8ce759b64dbe18780a12e0015fd002fc236f93f2
Victor Contreras <contreras3991@gmail.com>
2024-08-27 14:57:10 -0300

    added .gitignore file to the project

--- aula03/exercicios.py
+++ aula03/exercicios.py
@@ -187,6 +187,7 @@ from datetime import datetime
 ### Exercício 13. Consumo de API Simulado
 # Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
 
+
 # ### Exercício 14. Tentativas de Conexão
 # # Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
 
@@ -201,22 +202,22 @@ from datetime import datetime
 
 # print('Tentativas de conexão execedida')
 
-### Exercício 15. Processamento de Dados com Condição de Parada
-# Processar itens de uma lista até encontrar um valor específico que indica a parada.
+# ### Exercício 15. Processamento de Dados com Condição de Parada
+# # Processar itens de uma lista até encontrar um valor específico que indica a parada.
 
-itens = [
-    42, 3.14, "Hello", 99, 7.89, "World", 2024, 0.001, "Python",
-    -7, 2.718, "Coding", 1024, -3.14159, "ChatGPT", 18, 6.022e23,
-    "Data", 0, 9.81
-]
+# itens = [
+#     42, 3.14, "Hello", 99, 7.89, "World", 2024, 0.001, "Python",
+#     -7, 2.718, "Coding", 1024, -3.14159, "ChatGPT", 18, 6.022e23,
+#     "Data", 0, 9.81
+# ]
 
 
 
-i = 0
-while i < len(itens):
-    palavra = itens[i]
-    if palavra == 'Coding':
-        print(f'execução interrompida pela palavra {palavra}')
-        break  # Interrompe o loop `while` quando a palavra "sair" é encontrada
-    print(palavra)
-    i += 1  # Incrementa o índice para processar a próxima palavra
+# i = 0
+# while i < len(itens):
+#     palavra = itens[i]
+#     if palavra == 'Coding':
+#         print(f'execução interrompida pela palavra {palavra}')
+#         break  # Interrompe o loop `while` quando a palavra "sair" é encontrada
+#     print(palavra)
+#     i += 1  # Incrementa o índice para processar a próxima palavra
