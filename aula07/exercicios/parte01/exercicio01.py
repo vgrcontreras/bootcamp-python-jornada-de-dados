063fef44eda29f91754c7ac249876194167062b7
Victor Contreras <contreras3991@gmail.com>
2024-08-30 22:12:15 -0300

    aula07: exercicio01 resolvido

---
+++ aula07/exercicios/parte01/exercicio01.py
@@ -0,0 +1,8 @@
+# Calcular Média de Valores em uma Lista
+
+lista = [2,5,4,7,41,14,5,2,87,87,1]
+
+def sum_list(num_list: list) -> list:
+    return sum(num_list) / len(num_list) 
+
+print(sum_list(lista))     
