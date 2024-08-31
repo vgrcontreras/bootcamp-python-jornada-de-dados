031df5ca74156e7ff2fabcf98e1f09391ee5cc4e
Victor Contreras <contreras3991@gmail.com>
2024-08-30 22:13:08 -0300

    aula03: exercicio 04 resolvido

---
+++ aula07/exercicios/parte01/exercicio04.py
@@ -0,0 +1,6 @@
+# Converter Celsius para Fahrenheit em uma Lista
+
+def convert_celsius_to_fahrenheit(celsius: int) -> int:
+    return int((celsius * 1.8) + 32)
+
+print(convert_celsius_to_fahrenheit(20))
